46664ee67af0125c3ad290671518aae40fb05a9b
pighqs <peggy.hqs@gmail.com>
2017-10-30 17:50:42 +0100

    change cities on load to webservice

--- server.js
+++ server.js
@@ -9,27 +9,28 @@ app.set('view engine', 'ejs');
 // module request permet d'interroger webservice
 var request = require('request');
 
-var cityList = [
-    { name: "Paris", icone: "http://openweathermap.org/img/w/01d.png", description: "youpi", tempMax: "18 °C", tempMin: "14 °C" },
-    { name: "Lyon", icone: "http://openweathermap.org/img/w/11d.png", description: "caca", tempMax: "10 °C", tempMin: "2 °C" },
-    { name: "Marseille", icone: "http://openweathermap.org/img/w/03d.png", description: "bof", tempMax: "20 °C", tempMin: "16 °C" }
+var cityList = [];
 
-];
+var villesHome = ["Paris", "San Francisco", "Tokyo"];
 
-// var info = {
-//     "coord": { "lon": -122.42, "lat": 37.77 },
-//     "weather": [{ "id": 701, "main": "Mist", "description": "brume", "icon": "50n" }],
-//     "base": "stations",
-//     "main": { "temp": 12.5, "pressure": 1012, "humidity": 82, "temp_min": 11, "temp_max": 14 },
-//     "visibility": 16093,
-//     "wind": { "speed": 3.6, "deg": 250 },
-//     "clouds": { "all": 90 },
-//     "dt": 1509358020,
-//     "sys": { "type": 1, "id": 478, "message": 0.0047, "country": "US", "sunrise": 1509374025, "sunset": 1509412336 },
-//     "id": 0,
-//     "name": "San Francisco",
-//     "cod": 200
-// };
+for (var i = 0; i < villesHome.length; i++) {
+    //la ville demandée en url via le formulaire à openweathermap est stockée dans une variable
+    var villeHomeUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + villesHome[i] + "&appid=06aea259ff3cfc440c58bc8e256393c6&lang=fr&units=metric&type=accurate&mode=json";
+    request(villeHomeUrl, function(error, response, body) {
+        // la requête nous renvoie les infos qui seront stockées au format JSON dans une variable "body":
+        var body = JSON.parse(body);
+        // on récupère les infos de body pour assigner une nouvelle variable newCity
+        var ville = {
+            name: body.name,
+            icone: "http://openweathermap.org/img/w/" + body.weather[0].icon + ".png",
+            description: body.weather[0].description,
+            tempMax: body.main.temp_max + " °C",
+            tempMin: body.main.temp_min + " °C"
+        };
+        // on pushe cette variable dans le tableau cityList qui est lu par le front
+        cityList.push(ville);
+    });
+}
 
 
 // ROUTES
@@ -47,6 +48,7 @@ app.get('/add', function(req, res) {
         var meteoUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&lang=fr&units=metric&type=accurate&mode=json";
 
         request(meteoUrl, function(error, response, body) {
+
             // la requête nous renvoie les infos qui seront stockées au format JSON dans une variable "body":
             var body = JSON.parse(body);
             // on récupère les infos de body pour assigner une nouvelle variable newCity
@@ -60,12 +62,11 @@ app.get('/add', function(req, res) {
 
             // on pushe cette variable dans le tableau cityList qui est lu par le front
             cityList.push(newCity);
-            req.query = "";
+
             // on affiche la home
             res.render('home', { cityList: cityList });
 
         });
-
     }
 });
 
@@ -85,5 +86,5 @@ app.get('/delete', function(req, res) {
 var port = (process.env.PORT || 8080);
 
 app.listen(port, function() {
-    console.log("server.js chargé");
+    console.log("server listening on port 8080");
 });
